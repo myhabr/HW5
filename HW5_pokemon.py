8bd40363f263ac5451ea053a4ed86766d3ae92fc
myhabr <myhabr@gmail.com>
2017-11-30 23:22:08 +1000

    Лаба с покемонами, корректировки
    1

--- HW5_pokemon.py
+++ HW5_pokemon.py
@@ -6,16 +6,16 @@ from urllib.request import Request, urlopen
 from json import loads
 
 url = 'http://pokeapi.co/api/v2/pokemon/'
-pok_numb = input("Ведите ID покемона (целое число) от 1 до 802: \n")
+pok_numb = input("Ведите ID покемона (целое число): \n")
 req = Request(url + pok_numb, headers={'User-Agent': 'Mozilla/5.0'})
 resp = urlopen(req)
 res = loads(resp.read().decode('utf-8'))
 
-print("Найденный покемон")
+print("Найден покемон: ")
 print('Имя покемона: {}'.format(res['name']))
 print('Масса: {}'.format(res['weight']))
 print('Рост: {}'.format(res['height']) + '\n')
 print('Способности:')
 
 for ab_pok in res['abilities']:
-    print("Название способности: {}\n Умеет ли скрываться: {}\n".format(ab_pok['ability']['name'], ab_pok['is_hidden']))
+    print("Название способности: {}\n Имеет ли инвиз: {}\n".format(ab_pok['ability']['name'], ab_pok['is_hidden']))
