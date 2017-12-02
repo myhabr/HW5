8797868ae6195bdd6757fad8e7623ef331fb2ac3
myhabr <myhabr@gmail.com>
2017-12-02 14:21:24 +1000

    Добавил обработку исключений
    крутая тема

--- HW5_pokemon.py
+++ HW5_pokemon.py
@@ -4,18 +4,25 @@
 
 from urllib.request import Request, urlopen
 from json import loads
+from urllib.error import URLError, HTTPError
 
 url = 'http://pokeapi.co/api/v2/pokemon/'
 pok_numb = input("Ведите ID покемона (целое число): \n")
 req = Request(url + pok_numb, headers={'User-Agent': 'Mozilla/5.0'})
-resp = urlopen(req)
-res = loads(resp.read().decode('utf-8'))
+try:
+    resp = urlopen(req)
+except HTTPError as err1:
+    print("Ошибка HTTPError: ", err1)
+except URLError as err2:
+    print("Ошибка URLError: ", err2)
+else:
+    res = loads(resp.read().decode('utf-8'))
 
-print("Найден покемон: ")
-print('Имя покемона: {}'.format(res['name']))
-print('Масса: {}'.format(res['weight']))
-print('Рост: {}'.format(res['height']) + '\n')
-print('Способности:')
+    print("Найден покемон: ")
+    print('Имя покемона: {}'.format(res['name']))
+    print('Масса: {}'.format(res['weight']))
+    print('Рост: {}'.format(res['height']) + '\n')
+    print('Способности:')
 
-for ab_pok in res['abilities']:
-    print("Название способности: {}\n Имеет ли инвиз: {}\n".format(ab_pok['ability']['name'], ab_pok['is_hidden']))
+    for ab_pok in res['abilities']:
+        print("Название способности: {}\n Имеет ли инвиз: {}\n".format(ab_pok['ability']['name'], ab_pok['is_hidden']))
