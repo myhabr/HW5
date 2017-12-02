87d69dcf81aaf2eeb312035c2eac74d4c30b019e
myhabr <myhabr@gmail.com>
2017-12-02 14:24:39 +1000

    Добавил обработку исключений
    1

--- HW5_ISS_CUR_LOCATION.py
+++ HW5_ISS_CUR_LOCATION.py
@@ -4,10 +4,15 @@
 
 from urllib.request import Request, urlopen
 from json import loads
+from urllib.error import URLError, HTTPError
 
 # code for python 3.5
 req = Request('http://api.open-notify.org/iss-now.json')
-resp = urlopen(req)
-res = loads(resp.read().decode('utf-8'))
-print('Тек. местоположение МКС \n Широта: {} \n Долгота: {}'.format(res['iss_position']['latitude'],
+try:
+    resp = urlopen(req)
+except URLError as err2:
+    print("Ошибка URLError: ", err2)
+else:
+    res = loads(resp.read().decode('utf-8'))
+    print('Тек. местоположение МКС \n Широта: {} \n Долгота: {}'.format(res['iss_position']['latitude'],
                                                                         res['iss_position']['longitude']))
